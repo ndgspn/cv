44371f6eac1db5110f325a7728b0329462e0d82c
bueuk <bueuk@outlook.com>
2019-01-16 13:19:48 +0700

    deploy tes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,8 @@ set :deploy_to, "/home/nan/sopyan_id"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/master.key"
+#append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
