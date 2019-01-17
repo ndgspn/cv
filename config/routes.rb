c7f9c8c000fdd1ce751261109a6cc4af5bff4c34
bueuk <bueuk@outlook.com>
2019-01-17 21:26:53 +0700

    change root_path and menu position

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'profiles#index'
+  root to: 'posts#index'
 
   get 'login',  to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
